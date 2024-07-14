ca62e61ed1fc4d2a5210ba4888d4de3588535bc9
ashburndev <ashburndev@gmail.com>
2024-07-14 18:38:50 -0400

    initial commit

---
+++ src/main/java/ashburncode/asynchcapp/AsynchcappApplication.java
@@ -0,0 +1,13 @@
+package ashburncode.asynchcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchcappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchcappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/asynchcapp/AsynchcappApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.asynchcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
